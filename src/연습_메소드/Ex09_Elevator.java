62a9c0ee35455a60da6eece8fad750b02da1b99a
JS <smart317@naver.com>
2024-10-08 17:28:59 +0900

    메서드문제 수정

--- src/연습_메소드/Ex09_Elevator.java
+++ src/연습_메소드/Ex09_Elevator.java
@@ -4,21 +4,17 @@ public class Ex09_Elevator {
     public static void main(String[] args) {
         int a = 13;
         int b = 7;
-        int c = 16;
-
-        System.out.println(guide(a));
-        System.out.println(guide(b));
-        System.out.println(guide(c));
+        int c = 10;
+        System.out.printf("%d -> %s\n",a,guide(a));
+        System.out.printf("%d -> %s\n",b,guide(b));
+        System.out.printf("%d -> %s\n",c,guide(c));
     }
-
-    public static String guide(int a) {
-        String result = "";
-        if (a >= 11) {
-            result = "고층 엘베";
-        } else {
-            result = "저층 엘베";
-
+    public static String guide(int elv){
+        if (elv<=10){
+            return "저층 엘리베이터";
+        }else if (elv<=20){
+            return "고층엘리베이터";
         }
-        return result;
+        return "";
     }
 }
